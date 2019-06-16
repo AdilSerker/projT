5950e9ccb7541dce1f6281723a02ef903e8a86de
Adil Serkerov <adilserkerov@gmail.com>
2019-06-16 17:46:48 +0300

    ray caster

--- bt/Source/bt/Private/TankPlayerController.cpp
+++ bt/Source/bt/Private/TankPlayerController.cpp
@@ -10,7 +10,29 @@ ATank* ATankPlayerController::GetControllerTank() const {
 void ATankPlayerController::AimTowardsCroshair() {
 	if (!GetControllerTank()) { return; }
 
+	FVector hitLocation;
+	if (GetSightRayHitLocation(hitLocation)) {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *hitLocation.ToString())
+	}
+}
 
+bool ATankPlayerController::GetSightRayHitLocation(FVector& hitLcoation) const {
+	int32 vpSizeX, vpSizeY;
+	GetViewportSize(vpSizeX, vpSizeY);
+
+	FVector2D ScreenLocation(vpSizeX * CrossHairXLocation, vpSizeY * CrossHairYLocation);
+
+	FVector WordlLocation, WorldDirection;
+
+	bool b = DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WordlLocation, WorldDirection);
+
+	if (b) {
+		UE_LOG(LogTemp, Warning, TEXT("WordlLocation %s"), *WordlLocation.ToString())
+		UE_LOG(LogTemp, Warning, TEXT("WorldDirection %s"), *WorldDirection.ToString())
+	}
+		
+
+	return true;
 }
 
 void ATankPlayerController::BeginPlay() {
@@ -29,5 +51,5 @@ void ATankPlayerController::BeginPlay() {
 
 void ATankPlayerController::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
-
+	AimTowardsCroshair();
 }
--- bt/Source/bt/Public/TankPlayerController.h
+++ bt/Source/bt/Public/TankPlayerController.h
@@ -20,6 +20,15 @@ public:
 	void Tick(float DeltaTime) override;
 
 private:
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333;
+
 	ATank* GetControllerTank() const;
 	void AimTowardsCroshair();
+
+	bool GetSightRayHitLocation(FVector&) const;
 };
